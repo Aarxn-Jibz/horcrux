44201d6938acae34ae8429c59e444afc9edb3ce0
Aaron Jibin <aaron.jibin@btech.christuniversity.in>
2026-09-19 01:42:43 +0530

    test(identity): account for Windows file mode semantics

--- apps/node/internal/identity/identity_test.go
+++ apps/node/internal/identity/identity_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ed25519"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
@@ -28,7 +29,7 @@ func TestIdentityPersistsAndSigns(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if info.Mode().Perm() != 0o600 {
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
 		t.Fatalf("private identity permissions are %o", info.Mode().Perm())
 	}
 }
